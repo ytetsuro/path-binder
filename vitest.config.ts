5ba63770db11c26e415f33531cf83d2a75a24613
Tetsuro Yoshikawa <phper.0o0@gmail.com>
2026-03-06 13:12:25 +0900

    chore: setting ci/cd

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,11 @@ export default defineConfig({
   test: {
     globals: true,
     include: ['src/**/__tests__/**/*.test.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json-summary'],
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/__tests__/**'],
+    },
   },
 })
